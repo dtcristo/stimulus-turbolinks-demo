39e467dfdf527dbddc9114358e64c1bed1b568f9
David Cristofaro <david@dtcristo.com>
2018-04-01 11:41:52 +1000

    Add comments to form_controller

--- app/javascript/controllers/form_controller.ts
+++ app/javascript/controllers/form_controller.ts
@@ -4,10 +4,12 @@ export default class extends Controller {
   static targets = [ 'replace' ]
   replaceTarget: HTMLElement
 
+  // Triggered by `ajax:success` event on form
   response(e) {
     this.replaceTarget.outerHTML = e.detail[2].responseText
   }
 
+  // Triggered by `ajax:error` event on form
   exception(e) {
     alert('An unexpected error occured, please try again.')
   }
